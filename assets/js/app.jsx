78f97b33a23c70b7270db50880a63ada73fbc563
kasajei <sevenbluesea.yk@gmail.com>
2017-03-26 11:00:32 +0900

    API Callできた（ローカルだけ）

--- assets/js/app.jsx
+++ assets/js/app.jsx
@@ -1,7 +1,51 @@
 var React = require('react');
+var axios = require('axios');
+
+const host = window.location.host.split(':')[0];
+const ROOT_URL = (process.env.NODE_ENV == "prod" ? 'https://' + host : 'http://' + host+':8000') + '/api/v1';
+
+
+
+var Post = React.createClass({
+    render: function () {
+        return (
+            <div className="post">
+                <h1>{this.props.post.title}</h1>
+                <div className="body">
+                    {this.props.post.content}
+                </div>
+            </div>
+        )
+    }
+});
+
+
 
 module.exports = React.createClass({
+    getInitialState : function () {
+        return {posts:[]}
+    },
+    loadPosts(){
+        axios.get(ROOT_URL + "/posts").then(response =>{
+            this.setState({posts:response.data});
+        })
+    },
+    componentDidMount: function () {
+        this.loadPosts();
+    },
     render: function(){
-        return <h1>Hello, world@{process.env.NODE_ENV}サーバー</h1>
+        var Posts = this.state.posts.map(function (post) {
+            return <Post post={post} key={post.id}/>
+        });
+        return (
+            <div>
+                <header>
+                    <h1>Djangae + React Blog</h1>
+                </header>
+                <div id="content">
+                    {Posts}
+                </div>
+            </div>
+        );
     }
 });
